8fa286a5d6625d340a338041130c11977fe88902
farhan09999 <tfarhan068@gmail.com>
2025-10-21 22:06:43 +0600

    this is my first deployment V.0.0.1

--- myFirstProgramming.c
+++ myFirstProgramming.c
@@ -9,6 +9,14 @@ int main() {
     scanf("%d", &b);
 
     int sum = a + b;
-    printf("sum is : %d", sum);
+    int sub = a - b;
+    int mul = a * b;
+    int div = a / b;
+    int mod = a % b;
+    printf("sum is : %d ", sum);
+    printf("sub is : %d ", sub);
+    printf("mul is : %d ", mul);
+    printf("div is : %d ", div);
+    printf("mod is : %d ", mod);
     return 0;
 }  
